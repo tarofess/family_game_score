099bf8a08f925473ad419ec8f55bb580f4cc27c6
taro <tarofess@gmail.com>
2024-07-26 15:45:13 +0900

    ボタン連打でスナックバーを連続表示しないよう修正

--- lib/service/snackbar_service.dart
+++ lib/service/snackbar_service.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
 class SnackbarService {
-  void showHomeViewSnackBar(
-    BuildContext context,
-  ) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('プレイヤーが2名以上登録されていません\n設定画面でプレイヤーを追加してください',
-            style: TextStyle(fontWeight: FontWeight.bold)),
-      ),
-    );
+  bool isSnackbarVisible = false;
+
+  void showHomeViewSnackBar(BuildContext context) {
+    if (isSnackbarVisible) return;
+
+    isSnackbarVisible = true;
+
+    ScaffoldMessenger.of(context)
+        .showSnackBar(
+          const SnackBar(
+            content: Text('プレイヤーが2名以上登録されていません\n設定画面でプレイヤーを追加してください',
+                style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+        )
+        .closed
+        .then((_) => isSnackbarVisible = false);
+    ;
   }
 }
