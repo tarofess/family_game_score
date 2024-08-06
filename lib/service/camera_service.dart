f0d2f6a85a88c0880f0970643c3fa05a96969b18
taro <tarofess@gmail.com>
2024-08-06 09:59:00 +0900

    画像が削除されない不具合を修正

--- lib/service/camera_service.dart
+++ lib/service/camera_service.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:family_game_score/model/entity/player.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
@@ -25,8 +26,12 @@ class CameraService {
     }
   }
 
-  Future<String> saveImage(File imageFile) async {
+  Future<String> saveImage(File imageFile, Player? player) async {
     try {
+      if (await isImageExists(player?.image)) {
+        return player!.image;
+      }
+
       final Directory appDir = await getApplicationDocumentsDirectory();
       final String fileName = '${DateTime.now().millisecondsSinceEpoch}.jpg';
       final String filePath = path.join(appDir.path, fileName);
@@ -37,16 +42,44 @@ class CameraService {
     }
   }
 
+  Future<void> deleteImage(String? fileName) async {
+    try {
+      if (await isImageExists(fileName)) {
+        final File file = await getImageFileFromDocumentsDirectory(fileName);
+        await file.delete();
+      }
+    } catch (e) {
+      throw Exception('画像の削除中にエラーが発生しました');
+    }
+  }
+
   Future<Image?> getImageFromPath(String fileName) async {
     try {
-      final Directory appDir = await getApplicationDocumentsDirectory();
-      final String filePath = path.join(appDir.path, fileName);
-      final File file = File(filePath);
-
+      final File file = await getImageFileFromDocumentsDirectory(fileName);
       if (await file.exists()) {
         return Image.file(file);
       }
       return null;
+    } catch (e) {
+      throw Exception('画像の取得中にエラーが発生しました');
+    }
+  }
+
+  Future<bool> isImageExists(String? fileName) async {
+    try {
+      final File file = await getImageFileFromDocumentsDirectory(fileName);
+      return await file.exists();
+    } catch (e) {
+      throw Exception('画像の存在確認中にエラーが発生しました');
+    }
+  }
+
+  Future<File> getImageFileFromDocumentsDirectory(String? fileName) async {
+    try {
+      final Directory appDir = await getApplicationDocumentsDirectory();
+      final String filePath = path.join(appDir.path, fileName);
+      final File file = File(filePath);
+      return file;
     } catch (e) {
       throw Exception('画像の取得中にエラーが発生しました');
     }
