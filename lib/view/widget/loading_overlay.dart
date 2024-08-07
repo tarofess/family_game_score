b6c0ff77566ff2c83eacc7c1cb459980510f7132
taro <tarofess@gmail.com>
2024-08-07 21:52:44 +0900

    画像の保存時にサイズを圧縮して保存するよう修正

---
+++ lib/view/widget/loading_overlay.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class LoadingOverlay {
+  BuildContext _context;
+
+  void hide() {
+    Navigator.of(_context).pop();
+  }
+
+  void show() {
+    showDialog(
+      context: _context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return const PopScope(
+          canPop: false,
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<T> during<T>(Future<T> future) {
+    show();
+    return future.whenComplete(() => hide());
+  }
+
+  LoadingOverlay._create(this._context);
+
+  factory LoadingOverlay.of(BuildContext context) {
+    return LoadingOverlay._create(context);
+  }
+}
