73065c2828dc187c841f6f37f82eb1fd3c932a82
taro <tarofess@gmail.com>
2025-01-13 10:21:34 +0900

    ranking_viewmodelの削除

--- lib/presentation/view/ranking_view.dart
+++ lib/presentation/view/ranking_view.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:in_app_review/in_app_review.dart';
 
 import 'package:family_game_score/main.dart';
 import 'package:family_game_score/domain/entity/result.dart';
 import 'package:family_game_score/infrastructure/service/dialog_service.dart';
-import 'package:family_game_score/application/state/player_notifier.dart';
-import 'package:family_game_score/application/state/result_notifier.dart';
-import 'package:family_game_score/presentation/widget/common_async_widget.dart';
-import 'package:family_game_score/others/viewmodel/ranking_viewmodel.dart';
+import 'package:family_game_score/application/state/combined_provider.dart';
+import 'package:family_game_score/domain/entity/player.dart';
+import 'package:family_game_score/domain/entity/session.dart';
+import 'package:family_game_score/presentation/widget/async_error_widget.dart';
+import 'package:family_game_score/presentation/widget/list_card/result_list_card.dart';
+import 'package:family_game_score/presentation/widget/sakura_animation.dart';
 
 class RankingView extends HookConsumerWidget {
   final DialogService dialogService = getIt<DialogService>();
@@ -20,100 +20,106 @@ class RankingView extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final vm = ref.watch(rankingViewModelProvider);
-    final isVisibleFloatingActionButton = useState(vm.isFinishedGame());
+    final combinedState = ref.watch(combinedProvider);
 
-    useEffect(() {
-      Future.microtask(() async {
-        try {
-          final InAppReview inAppReview = InAppReview.instance;
-          if (await vm.shouldShowInAppReview(inAppReview)) {
-            await inAppReview.requestReview();
-          }
-          // ignore: empty_catches
-        } catch (e) {}
-      });
-      return null;
-    }, []);
+    return combinedState.when(
+      data: (combinedData) {
+        final session = combinedData.$1;
+        final players = combinedData.$2;
+        final results = combinedData.$3;
 
-    return Scaffold(
-      appBar: buildAppBar(context, ref, vm),
-      body: buildBody(context, ref, vm),
-      floatingActionButton: buildFloatingActionButton(
-          context, ref, isVisibleFloatingActionButton),
+        return _buildScaffold(context, ref, session, players, results);
+      },
+      loading: () {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+      error: (error, stackTrace) {
+        return AsyncErrorWidget(error: error, retry: () => combinedState);
+      },
     );
   }
 
-  AppBar buildAppBar(BuildContext context, WidgetRef ref, RankingViewModel vm) {
-    return AppBar(
-      centerTitle: true,
-      title: Text(vm.getAppBarTitle(), style: TextStyle(fontSize: 20.sp)),
-      toolbarHeight: 56.r,
-      actions: [
-        vm.getIconButton(
-          () async {
+  Widget _buildScaffold(
+    BuildContext context,
+    WidgetRef ref,
+    Session? session,
+    List<Player> players,
+    List<Result> results,
+  ) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+            session != null && session.endTime == null ? '現在の順位' : '結果発表',
+            style: TextStyle(fontSize: 20.sp)),
+        toolbarHeight: 56.r,
+        actions: [
+          session != null && session.endTime == null
+              ? const SizedBox()
+              : IconButton(
+                  icon: Icon(Icons.home, size: 24.r),
+                  onPressed: () async {
+                    final isSuccess = await dialogService
+                        .showReturnToHomeDialog(context, ref);
+                    if (isSuccess) {
+                      if (context.mounted) {
+                        context.pushReplacement('/');
+                      }
+                    }
+                  },
+                ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          _buildRankingList(players, results, context, ref),
+          _buildSakuraAnimation(session),
+        ],
+      ),
+      floatingActionButton: Visibility(
+        visible: session != null && session.endTime != null,
+        child: FloatingActionButton(
+          onPressed: () async {
             final isSuccess =
-                await dialogService.showReturnToHomeDialog(context, ref);
+                await dialogService.showAddGameTypeDialog(context, ref);
             if (isSuccess) {
               if (context.mounted) {
-                context.pushReplacement('/');
+                await dialogService.showMessageDialog(
+                    context, 'ゲームの種類を記録しました。');
               }
             }
           },
-        )
-      ],
-    );
-  }
-
-  Widget buildBody(BuildContext context, WidgetRef ref, RankingViewModel vm) {
-    return Stack(
-      children: [
-        vm.results.when(
-          data: (data) {
-            return buildRankingList(data, vm, context, ref);
-          },
-          loading: () => CommonAsyncWidgets.showLoading(),
-          error: (error, stackTrace) =>
-              CommonAsyncWidgets.showDataFetchErrorMessage(
-                  context, ref, resultNotifierProvider, error),
+          child: Icon(Icons.mode_edit, size: 24.r),
         ),
-        vm.getSakuraAnimation(),
-      ],
+      ),
     );
   }
 
-  Widget buildRankingList(List<Result> results, RankingViewModel vm,
+  Widget _buildRankingList(List<Player> players, List<Result> results,
       BuildContext context, WidgetRef ref) {
-    return vm.activePlayers.when(
-      data: (data) => ListView.builder(
-        itemCount: results.length,
-        itemBuilder: (context, index) =>
-            vm.getResultListCard(index, results, data),
-      ),
-      loading: () => CommonAsyncWidgets.showLoading(),
-      error: (error, stackTrace) =>
-          CommonAsyncWidgets.showDataFetchErrorMessage(
-              context, ref, playerNotifierProvider, error),
+    return ListView.builder(
+      itemCount: results.length,
+      itemBuilder: (context, index) {
+        final result = results[index];
+        final player = players.firstWhere((p) => p.id == result.playerId);
+        return ResultListCard(
+          key: ValueKey(player.id),
+          player: player,
+          result: result,
+        );
+      },
     );
   }
 
-  Visibility buildFloatingActionButton(BuildContext context, WidgetRef ref,
-      ValueNotifier<bool> isVisibleFloatingActionButton) {
-    return Visibility(
-      visible: isVisibleFloatingActionButton.value,
-      child: FloatingActionButton(
-        onPressed: () async {
-          final isSuccess =
-              await dialogService.showAddGameTypeDialog(context, ref);
-          if (isSuccess) {
-            if (context.mounted) {
-              await dialogService.showMessageDialog(context, 'ゲームの種類を記録しました。');
-            }
-            isVisibleFloatingActionButton.value = false;
-          }
-        },
-        child: Icon(Icons.mode_edit, size: 24.r),
-      ),
-    );
+  Widget _buildSakuraAnimation(Session? session) {
+    return session != null && session.endTime == null
+        ? const SizedBox()
+        : const Positioned.fill(
+            child: IgnorePointer(
+              child: SakuraAnimation(),
+            ),
+          );
   }
 }
